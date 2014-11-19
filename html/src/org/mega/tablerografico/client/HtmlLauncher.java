008474f1e7618d485c39cce401166b36e1acfade
Ignacio Losiggio iglosiggio <iglosiggio@gmail.com>
2014-11-19 11:58:17 -0300

    Init

---
+++ html/src/org/mega/tablerografico/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.mega.tablerografico.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.mega.tablerografico.PuntoEntrada;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new PuntoEntrada();
+        }
+}
