88f1fe7e1c8a7e802a7750026fc5fbd92cce1c8e
Ignacio Losiggio iglosiggio <iglosiggio@gmail.com>
2014-11-26 21:55:26 -0300

    A bit of improval

--- desktop/src/org/mega/tablerografico/desktop/DesktopLauncher.java
+++ desktop/src/org/mega/tablerografico/desktop/DesktopLauncher.java
@@ -1,17 +1,18 @@
 package org.mega.tablerografico.desktop;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import org.mega.tablerografico.PuntoEntrada;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
+	DisplayMode display = LwjglApplicationConfiguration.getDesktopDisplayMode();;
 	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 	config.fullscreen = true;
-	//config.width = 1024;
-	//config.height = 600;
-	config.width = 800;
-	config.height = 600;
+	config.width = display.width;
+	config.height = display.height;
 	config.title = "Carteado!";
 	new LwjglApplication(new PuntoEntrada(), config);
     }
